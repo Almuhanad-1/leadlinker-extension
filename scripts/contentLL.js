219d8f8d70227292aa002e66d08b5ba904821c09
Almuhanad-1 <almuhanad.alnihmy@gmail.com>
2023-12-06 23:26:14 +0300

    new version with new approuch

--- scripts/contentLL.js
+++ scripts/contentLL.js
@@ -1,56 +1,69 @@
-function replacePhoneNumbersWithLeadlinker(htmlContent) {
-  // Phone number regex
-  const phoneRegex = /(?:\+|00)[1-9]\d{0,3}[\s-]?(?:(?:\(\d{1,5}\)[\s-]?)?[\d\s-]{5,}|(?:\d[\s-]?){8,})/g;
-
-  // Find all matches
-  const matches = htmlContent.match(phoneRegex);
-
-  if (!matches) {
-    return htmlContent;
-  }
-
-  // Filter out matches within SVG, PATH, PICTURE tags
-  const filteredMatches = matches
-    .filter((match) => {
-      const excludedTagsRegex = /<svg.*?<\/svg>|<path.*?<\/path>|<picture.*?<\/picture>/g;
-      const excludedMatches = htmlContent.match(excludedTagsRegex);
-      return excludedMatches ? excludedMatches.every((tag) => !tag.includes(match)) : true;
-    });
-
-  // Replace phone numbers with anchor tags using the filtered matches
-  const replacedContent = htmlContent.replace(phoneRegex, (match) => {
-    if (filteredMatches.includes(match)) {
-      // Format the phone number as "+1234567890" without spaces, parentheses, dots, or dashes
-      const formattedNumber = match.replace(/[\s\(\).-]/g, '');
-
-      // Use the formatted number inside the WhatsApp anchor tag
-      return `${match}<a href="https://api.whatsapp.com/send?phone=${formattedNumber}" target="_blank"><span class="material-symbols-outlined">link</span></a>`;
-    } else {
-      return match;
-    }
-  });
-
-  return replacedContent;
+function addFontIconToHead() {
+  const linkElement = document.createElement('link');
+  linkElement.rel = 'stylesheet';
+  linkElement.href = 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0';
+  const headElement = document.head || document.getElementsByTagName('head')[0];
+  headElement.appendChild(linkElement);
 }
 
-// add font icon to head
-const linkElement = document.createElement('link');
-linkElement.rel = 'stylesheet';
-linkElement.href = 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0';
-const headElement = document.head || document.getElementsByTagName('head')[0];
-headElement.appendChild(linkElement);
+function insertPhoneNumbers(textContent, phoneRegex) {
+  return textContent.replace(phoneRegex, (match) => {
+    // Format the phone number as "+1234567890" without spaces, parentheses, dots, or dashes
+    const formattedNumber = match.replace(/[\s\(\).-]/g, '');
+
+    // anchor element with whatsapp link
+    const anchorElement = document.createElement('a');
+    anchorElement.href = `https://api.whatsapp.com/send?phone=${formattedNumber}`;
+    anchorElement.target = '_blank';
+
+    // span element for icon
+    const spanElement = document.createElement('span');
+    spanElement.className = 'material-symbols-outlined';
+    spanElement.textContent = 'link';
+
+    anchorElement.appendChild(spanElement);
+
+    return `${match} ${anchorElement.outerHTML} `;
+  });
+}
+
+function insertPhoneNumbersWithLeadlinker(allTagsInBody) {
+  const phoneRegex = /(?:\+|00)[1-9]\d{0,3}[\s-]?(?:(?:\(\d{1,5}\)[\s-]?)?[\d\s-]{5,}|(?:\d[\s-]?){8,})/g;
+
+  allTagsInBody.forEach((tag) => {
+    if (tag.hasChildNodes()) {
+      tag.childNodes.forEach((child) => {
+        if (child.nodeType === Node.TEXT_NODE) {
+          const replacedContent = insertPhoneNumbers(child.textContent, phoneRegex);
+          const tempElement = document.createElement('div');
+          tempElement.innerHTML = replacedContent;
+          while (tempElement.firstChild) {
+            tag.insertBefore(tempElement.firstChild, child);
+          }
+          tag.removeChild(child);
+        }
+      });
+    } else if (tag.nodeType === Node.TEXT_NODE) {
+      const replacedContent = insertPhoneNumbers(tag.textContent, phoneRegex);
+      const tempElement = document.createElement('div');
+      tempElement.innerHTML = replacedContent;
+      while (tempElement.firstChild) {
+        tag.parentNode.insertBefore(tempElement.firstChild, tag);
+      }
+      tag.parentNode.removeChild(tag);
+    }
+  });
+}
 
 
-console.log('file loaded #########');
 
 window.onload = function () {
-  console.log('Page fully loaded');
-  const bodyContent = document.body.innerHTML;
-  const replacedContent = replacePhoneNumbersWithLeadlinker(bodyContent);
-  document.body.innerHTML = replacedContent;
+  const bodyElement = document.body;
+  const allTagsInBody = Array.from(bodyElement.getElementsByTagName('*'));
+  insertPhoneNumbersWithLeadlinker(allTagsInBody)
+  addFontIconToHead();
 };
 
-// alert('DOM loadedqq');
 chrome.runtime.sendMessage({ type: 'example' }, function (response) {
   console.log('Response received:', response);
 });
